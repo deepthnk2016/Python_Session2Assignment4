d0e83dddd427d8bfe9f9dbfd66b975c82fc47222
Deepak Ray <deepthnk2016@gmail.com>
2018-08-12 23:00:11 +0530

    Add files via upload

---
+++ Session2Assignment4.py
@@ -0,0 +1,11 @@
+#-----------------------------------------------------------------
+# Python - Session 2 Assignment 4
+# Descr - Assign a multiline string to a variable and print it
+#-----------------------------------------------------------------
+
+mystring = """WE, THE PEOPLE OF INDIA,
+      having solemnly resolved to constitute India into a SOVEREIGN, !
+            SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC
+             and to secure to all its citizens:"""
+
+print(mystring)
